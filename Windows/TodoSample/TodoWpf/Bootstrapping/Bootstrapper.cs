e99e645e3dcaee84ec86a4abd7435842c80e7d11
variar <anton.filimonov@gmail.com>
2015-12-23 03:27:19 -0800

    TodoSample for windows

---
+++ Windows/TodoSample/TodoWpf/Bootstrapping/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using TodoWpf.Services;
+using TodoWpf.View;
+using TodoWpf.ViewModel;
+
+namespace TodoWpf.Bootstrapping
+{
+    public sealed class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            var shell = Container.Resolve<MainWindow>();
+            shell.Show();
+
+            return shell;
+        }
+        
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType<TodoManager>(new ContainerControlledLifetimeManager());
+            Container.RegisterType<MainWindowViewModel>();
+            Container.RegisterType<MainWindow>(new InjectionFactory(c => new MainWindow { DataContext = c.Resolve<MainWindowViewModel>() }));
+        }
+    }
+}
