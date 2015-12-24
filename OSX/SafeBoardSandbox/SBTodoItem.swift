7e1b52dfcbb5eb4fafc5a86c8a18b7fce39ba9b4
Vladimir Kukushkin <kukushechkin@mac.com>
2015-12-24 19:22:07 +0300

    Move back

---
+++ OSX/SafeBoardSandbox/SBTodoItem.swift
@@ -0,0 +1,9 @@
+//
+//  SBTodoItem.swift
+//  SafeBoardSandbox
+//
+//  Created by Vladimir Kukushkin on 12/24/15.
+//  Copyright © 2015 Vladimir Kukushkin. All rights reserved.
+//
+
+import Foundation
